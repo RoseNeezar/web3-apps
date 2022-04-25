fb3aca68418a48b3fca08d99a92df0ce41d2da18
Neezar <rforlyffe@gmaicl.com>
2022-04-25 23:06:32 +0800

    add minting app

---
+++ packages/minting/dapp.config.js
@@ -0,0 +1,9 @@
+const config = {
+  title: 'Bored_App Dapp',
+  description: 'A showcase dapp',
+  contractAddress: '0x0312e42e4b55823a3C41769DC0B07F382dECc247',
+  maxMintAmount: 10,
+  presaleMaxMintAmount: 3,
+  price: 0.01,
+}
+export { config }
